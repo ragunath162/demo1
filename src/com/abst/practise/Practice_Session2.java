346a39f685fbb1dc6f6215370c03fbf7f23d5abd
ragunath162 <ragunathragu162@gmail.com>
2022-11-17 18:18:25 +0530

    java added

---
+++ src/com/abst/practise/Practice_Session2.java
@@ -0,0 +1,15 @@
+package com.abst.practise;
+
+public class Practice_Session2 extends Practise_Session {
+
+	@Override
+	public void raju() {
+System.out.println("the raju");		
+	}
+
+	public static void main(String[] args) {
+		Practice_Session2 hello= new Practice_Session2();
+		hello.ragu();
+		hello.raju();
+	}
+}
---
+++ src/com/abst/practise/Practise_Session.java
@@ -0,0 +1,10 @@
+package com.abst.practise;
+
+public abstract class Practise_Session {  
+	
+	public void ragu() {
+System.out.println("the ragu");
+	}
+ 
+	public abstract void raju();
+}
